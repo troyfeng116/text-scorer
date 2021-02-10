eea6e69e8b7f3abf8e6462a6ade372ab769fc757
Troy Feng <troy.feng@yale.edu>
2021-02-10 00:13:44 -0500

    Rm old helpers + test scripts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,3 @@
-export const isAlphaOrSpace = (charCode: number): boolean => {
-    return (97 <= charCode && charCode <= 122) || charCode == 32
-}
-
-export const charCodeToIndex = (charCode: number): number => {
-    if (charCode === 32) return 26
-    return charCode - 97
-}
-
 // To do: clean training text based on allowed chars
 export const cleanTrainingText = (text: string): string => {
     return text.toLowerCase().replace(/\.|\?|\!|\,|\&/gi, '')
